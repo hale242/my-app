1c92333e9ab96d72dce76c4c6b9cee74102039b5
hale242 <56573304+hale242@users.noreply.github.com>
2024-03-21 13:14:44 +0700

    set dynamic title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [pageTitle, setPageTitle] = useState("Default Title");
+
+  useEffect(() => {
+    setPageTitle("Dynamic Page Title");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <title>{pageTitle}</title>
+      </Helmet>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
